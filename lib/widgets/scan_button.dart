25a2241da40becaa72239ca64031a838de6f3244
Juan David Garzon Vallejo <juanchope039@gmail.com>
2025-10-25 15:10:44 -0500

    mi primer commit clase 25-10-2025
    Signed-off-by: Juan David Garzon Vallejo <juanchope039@gmail.com>

---
+++ lib/widgets/scan_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ScanButton extends StatelessWidget {
+  const ScanButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      elevation: 0,
+      child: Icon(Icons.filter_center_focus),
+
+      onPressed: () async {
+
+        /*
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#3D8BEF',
+          'Cancelar',
+          false,
+          ScanMode.QR,
+        );
+        // final barcodeScanRes = 'https://fernando-herrera.com';
+        // final barcodeScanRes = 'geo:45.287135,-75.920226';
+
+        if (barcodeScanRes == '-1') {
+          return;
+        }
+
+        final scanListProvider = Provider.of<ScanListProvider>(
+          context,
+          listen: false,
+        );
+
+        final nuevoScan = await scanListProvider.nuevoScan(barcodeScanRes);
+
+        launchURL(context, nuevoScan);
+        */
+      },
+    );
+  }
+}

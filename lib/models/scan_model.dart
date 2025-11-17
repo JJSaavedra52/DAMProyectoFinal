392483a57157768fd076c0118162fa9673ccd5f8
Juan D Garzon Vallejo <juanchope039@gmail.com>
2025-11-17 00:30:35 -0500

    rebase-3
    Signed-off-by: Juan D Garzon Vallejo <juanchope039@gmail.com>

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -58,27 +58,32 @@ class ScanModel {
     int? id;
     String? tipo;
     String valor;
-
+    String? location;
    
     ScanModel({
         this.id,
         this.tipo,
+        location,
         required this.valor,
     }) {
 
       if ( valor.contains('http') ) {
         tipo = 'http';
-      } 
-      else { 
-         if ( valor.contains('geo') ) {
-           tipo = 'geo';
-         }
-         else{
-           tipo = 'otro';
-         }
+      } else if ( valor.contains('geo') ) {
+         tipo = 'geo';
+      } else{
+        tipo = 'otro';
+        this.location = location;
       }
     }
-    
+
+    LatLng getLocation(){
+      final latLng = location!.substring(4).split(',');
+      final lat = double.parse( latLng[0] );
+      final lng = double.parse( latLng[1] );
+
+      return LatLng( lat, lng );
+    }
 
     LatLng getLatLng() {
 
@@ -93,12 +98,14 @@ class ScanModel {
         id: json["id"],
         tipo: json["tipo"],
         valor: json["valor"],
+        location: json["location"],
     );
 
     Map<String, dynamic> toJson() => {
         "id": id,
         "tipo": tipo,
         "valor": valor,
+        "location": location,
     };
 
 }
